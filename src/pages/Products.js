d785f117b1733fd2a4932ea58ee77c423be6265a
Sepanta <Sepanta_97@Yahoo.com>
2021-10-03 17:32:10 +0330

    adding Product Item styles

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,9 +1,20 @@
 import React from "react";
-
+import ProductItem from "../components/ProductsItem/ProductItem";
+import products from "../products";
 const Products = () => {
   return (
-    <section className="mt-5 text-center">
-      <h1>Products</h1>
+    <section class="text-gray-600 body-font">
+      <div class="container px-5 py-24 mx-auto">
+        {
+          products.map(product=>(
+            <div class="flex flex-wrap -m-4">
+        <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
+         <ProductItem key={product.id} price={product.price} description={product.description} image={product.image} />
+        </div>
+        </div>
+          ))
+        }
+      </div>
     </section>
   );
 };
