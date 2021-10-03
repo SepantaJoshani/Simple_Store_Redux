1c68275f38c4a5eb600e2f7fd5be7d331f81c3ab
Sepanta <Sepanta_97@Yahoo.com>
2021-10-03 23:01:25 +0330

    creating cart

---
+++ src/CartItem/CartItem.js
@@ -0,0 +1,56 @@
+import React from "react";
+import FavoriteIcon from "../components/Icons/FavoriteIcon";
+import MinusIcon from "../components/Icons/MinusIcon";
+import PlusIcon from "../components/Icons/PlusIcon";
+import RemoveIcon from "../components/Icons/RemoveIcon";
+
+const CartItem = ({name,image,price}) => {
+  return (
+    
+        <li className="flex flex-col py-6 sm:flex-row sm:justify-between">
+          <div className="flex w-full space-x-2 sm:space-x-4 md:w-8/12 md:mx-auto">
+            <img
+              className="flex-shrink-0 object-cover w-20 h-20 rounded outline-none dark:border-transparent sm:w-32 sm:h-32 dark:bg-coolGray-500"
+                src={image}
+              alt="Polaroid camera"
+            />
+            <div className="flex flex-col justify-between w-full pb-4">
+              <div className="flex justify-between w-full pb-2 space-x-2">
+                <div className="space-y-1">
+                  <h3 className="text-lg font-semibold leading-snug sm:pr-8">
+                    {name}
+                  </h3>
+                </div>
+                <div className="space-y-2 text-right">
+                  <p className="text-lg font-semibold">${price}</p>
+                  <div className="cursor-pointer">
+                  <PlusIcon/>
+                  </div>
+                 <div className="cursor-pointer">
+                 <MinusIcon />
+                 </div>
+                </div>
+              </div>
+              <div className="flex text-sm divide-x">
+                <button
+                  type="button"
+                  className="flex items-center px-2 py-1 pl-0 space-x-1"
+                >
+                  <RemoveIcon />
+                  <span>Remove</span>
+                </button>
+                <button
+                  type="button"
+                  className="flex items-center px-2 py-1 space-x-1"
+                >
+                  <span>X</span>
+                  <p className='text-lg'>3</p>
+                </button>
+              </div>
+            </div>
+          </div>
+        </li>
+  );
+};
+
+export default CartItem;
--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,10 +1,34 @@
 import React from 'react'
+import CartItem from '../CartItem/CartItem'
+import products from '../products'
 
 const Cart = () => {
+    
     return (
-        <h1>
-           Cart 
-        </h1>
+        <div className="flex flex-col p-6 space-y-4 sm:p-10 dark:bg-coolGray-900 dark:text-coolGray-100">
+      <h2 className="text-xl font-semibold">Your cart</h2>
+      <ul className="flex flex-col divide-y divide-coolGray-700">
+          {
+              products.map(product=>(
+                  <CartItem price={product.price} name={product.name} image={product.image} />
+              ))
+          }
+    </ul>
+    <div className="space-y-1 text-right sm:mx-auto sm:flex sm:flex-col sm:items-center">
+		<p>Total amount:
+			<span className="font-semibold sm:text-xl">$357</span>
+		</p>
+		<p className="text-sm dark:text-coolGray-400">Not including taxes and shipping costs</p>
+	</div>
+	<div className="flex justify-end space-x-4 sm:mx-auto md:space-x-9">
+		<button type="button" className="px-6 py-2 bg-blue-400 border rounded-md dark:border-violet-400">Back
+			<span className="sr-only sm:not-sr-only">to shop</span>
+		</button>
+		<button type="button" className="px-6 py-2 bg-blue-400 border rounded-md dark:bg-violet-400 dark:text-coolGray-900 dark:border-violet-400">
+			<span className="sr-only sm:not-sr-only">Continue to</span>Checkout
+		</button>
+	</div>
+    </div>
     )
 }
 
